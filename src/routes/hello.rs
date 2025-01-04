6e648f289564716fb96852b5c6f0e1234c50e171
Daniel Cunha <danielsoro@gmail.com>
2025-01-04 18:28:11 -0300

    feat(json): trying json extractor

--- src/routes/hello.rs
+++ src/routes/hello.rs
@@ -1,11 +1,26 @@
-use axum::{
-    response::Html,
-    routing::{get, Router},
-};
+use std::collections::HashMap;
 
+use axum::{
+    response::{Html, IntoResponse},
+    routing::{get, Router},
+    Json,
+};
+use log::info;
+
+async fn hello() -> impl IntoResponse {
+    info!("Calling hello route");
+    Html("<strong>Hello World</strong>")
+}
+
+async fn hello_json() -> impl IntoResponse {
+    info!("Calling json route");
+    Json(HashMap::from([
+        ("message", "Hello World".to_string()),
+        ("date", chrono::Utc::now().to_rfc3339()),
+    ]))
+}
 pub fn routes() -> Router {
-    Router::new().route(
-        "/hello",
-        get(|| async { Html("<strong>Hello World</strong>") }),
-    )
+    Router::new()
+        .route("/hello", get(hello))
+        .route("/json", get(hello_json))
 }

fbdd3b20c0fa8c8d7c75ad2ce7eb8fc8d08a5183
Daniel Cunha <danielsoro@gmail.com>
2025-10-12 11:39:28 -0300

    Adding json route

--- tests/hello_test.rs
+++ tests/hello_test.rs
@@ -4,6 +4,7 @@ use axum::{
 };
 use axum_course::routes;
 use http_body_util::BodyExt;
+use serde_json;
 use tower::ServiceExt;
 
 #[tokio::test]
@@ -23,4 +24,26 @@ async fn hello() {
     assert_eq!(response.status(), StatusCode::OK);
     let body = response.into_body().collect().await.unwrap().to_bytes();
     assert_eq!(body, Bytes::from("<strong>Hello World</strong>"));
+}
+
+#[tokio::test]
+async fn hello_json() {
+    let hello_routes = routes::hello::routes();
+    let response = hello_routes
+        .oneshot(
+            Request::builder()
+                .method(http::Method::GET)
+                .uri("/json")
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = response.into_body().collect().await.unwrap().to_bytes();
+    let json: serde_json::Value = serde_json::from_slice(&body).unwrap();
+
+    assert_eq!(json["message"], "Hello World");
+    assert!(json["date"].is_string());
 }

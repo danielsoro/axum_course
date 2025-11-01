a8dca9306cddf086a93c6e12a71ead59627c6195
Daniel Cunha <danielsoro@gmail.com>
2025-11-01 09:44:29 -0300

    Fixing tests

--- tests/hello_test.rs
+++ tests/hello_test.rs
@@ -1,15 +1,20 @@
+use std::sync::Arc;
+
 use axum::{
     body::{Body, Bytes},
     http::{self, Request, StatusCode},
 };
-use axum_course::hello;
+use axum_course::{hello, server::AppState};
 use http_body_util::BodyExt;
 use serde_json;
 use tower::ServiceExt;
 
 #[tokio::test]
 async fn hello() {
-    let hello_routes = hello::routes();
+    let app_sate = Arc::new(AppState {
+        message: "My State",
+    });
+    let hello_routes = hello::routes(app_sate);
     let response = hello_routes
         .oneshot(
             Request::builder()
@@ -28,12 +33,15 @@ async fn hello() {
 
 #[tokio::test]
 async fn hello_json() {
-    let hello_routes = hello::routes();
+    let app_sate = Arc::new(AppState {
+        message: "My State",
+    });
+    let hello_routes = hello::routes(app_sate);
     let response = hello_routes
         .oneshot(
             Request::builder()
                 .method(http::Method::GET)
-                .uri("/json")
+                .uri("/hello/Name")
                 .body(Body::empty())
                 .unwrap(),
         )
@@ -44,6 +52,6 @@ async fn hello_json() {
     let body = response.into_body().collect().await.unwrap().to_bytes();
     let json: serde_json::Value = serde_json::from_slice(&body).unwrap();
 
-    assert_eq!(json["message"], "Hello World");
+    assert_eq!(json["message"], "Hello Name");
     assert!(json["date"].is_string());
 }
